d5d3e79e8002b66c870a31b0ba59dc2309555284
NicoDevStuff <NicoG2008@gmx.de>
2023-04-04 23:47:17 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -18,7 +18,6 @@ end
 set -e fish_user_paths
 set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_paths
 
-#
 ####################### CUSTOMISATION ######################
 
 #remove greeting message
@@ -31,26 +30,20 @@ colorscript random
 
 alias clear="clear && colorscript random"
 alias vim="nvim"
-alias nvd="neovide"
 alias hx="helix"
-#faster make compiling
 alias make="make -j 24" 
 alias ..="cd .."
 alias home="cd ~"
-
-# nvim directory
-alias vimdir='cd ~/.config/nvim'
-# always ask to copy/move/remove something
 alias cp="cp -i"
 alias mv='mv -i'
 alias rm='rm -i'
 
 # Changing 'ls' to 'exa' so it looks good :)
-alias ls='exa -al --color=always --group-directories-first' # my preferred listing
-alias la='exa -a --color=always --group-directories-first'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first'  # long format
-alias lt='exa -aT --color=always --group-directories-first' # tree listing
-alias l.='exa -a | egrep "^\."'
+alias ls='exa -al --icons --color=always --group-directories-first' # my preferred listing
+alias la='exa -a --icons --color=always --group-directories-first'  # all files and dirs
+alias ll='exa -l --icons --color=always --group-directories-first'  # long format
+alias lt='exa -aT --icons --color=always --group-directories-first' # tree listing
+alias l.='exa --icons -a | egrep "^\."'
 
 #changing 'cat' to 'bat' for a fricking fancy look!
 alias cat='bat'
